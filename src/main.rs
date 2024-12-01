9cd2ed6602c97899e78a30b46ac318e1348cf9f5
Rick van der Wal <rick@tortoaster.com>
2024-12-01 12:10:18 +0100

    Solve 1a

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+const INPUT: &str = include_str!("../inputs/input1a.txt");
+
 fn main() {
-    println!("Hello, world!");
+    let (mut first, mut second): (Vec<i32>, Vec<i32>) = INPUT
+        .lines()
+        .map(|line| line.split_once("   ").unwrap())
+        .map(|(first, second)| {
+            (
+                first.parse::<i32>().unwrap(),
+                second.parse::<i32>().unwrap(),
+            )
+        })
+        .unzip();
+    first.sort();
+    second.sort();
+    let sum = first
+        .into_iter()
+        .zip(second)
+        .map(|(first, second)| first.abs_diff(second))
+        .sum::<u32>();
+    println!("{sum}");
 }
